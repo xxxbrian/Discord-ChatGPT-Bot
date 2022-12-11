fe87eed59ee25379445266cc4bead0aef3a600ff
Bojin Li <bojin.li@foxmail.com>
2022-12-11 19:35:11 +1100

    feat: Change to use slash commands

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,17 +1,41 @@
 from pychatgpt import Chat
 
 
-class ChatBot():
+class ChatThread():
 
-    def __init__(self, email, password, proxies: str or dict = None):
+    def __init__(self, email, password):
         self.email = email
         self.password = password
-        self.proxies = proxies
         self.threads = {}
-        self.dcbot_id = None
+        # initialize the ChatGPT API
+        Chat(email, password)
 
-    def set_dcbot_id(self, id):
-        self.dcbot_id = id
+    def chat_start(self, id: str) -> str:
+        if id in self.threads:
+            return '**Conversation already started.**'
+        self.threads[id] = Chat(self.email, self.password)
+        return '**Conversation started.**'
+
+    def chat_end(self, id: str) -> str:
+        if id not in self.threads:
+            return '**Conversation not started.**'
+        self.threads.pop(id)
+        return '**Conversation ended. I have forgotten everything.**'
+
+    def handle(self, id: str, message) -> list[str]:
+        if id not in self.threads:
+            return [
+                '**Use `/start` to start a conversation\nUse `/end` to end the conversation.\n\nI will forget everything after the conversation ends.**'
+            ]
+        try:
+            # response, conversation_id, previous_convo_id
+            response, _, _ = self.threads[id].ask(message)
+            resp_chunks = self._split_message(response)
+        except Exception as e:
+            print(e)
+            resp_chunks = ['*Wabibabu, wabibabu, wabibabu...*']
+        finally:
+            return resp_chunks
 
     @staticmethod
     def _split_message(message, size=1800) -> list:
@@ -25,7 +49,6 @@ class ChatBot():
                     shift = chunk.rfind(c)
                     if shift != -1:
                         break
-
                 shift = chunk.rfind(' ') if shift == -1 else shift
                 if shift != -1:
                     chunk = chunk[:shift + 1]
@@ -33,61 +56,3 @@ class ChatBot():
             message = message[len(chunk):]
         chunks.append(message)
         return chunks
-
-    async def handle_dm(self, message):
-        if message.author not in self.threads:
-            if message.content != '!start':
-                await message.channel.send(
-                    '**Send `!start` to start a conversation\nSend `!end` to end the conversation.\n\nI will forget everything after the conversation ends.**'
-                )
-                return
-            self.threads[message.author] = Chat(self.email,
-                                                self.password,
-                                                proxies=self.proxies)
-            await message.channel.send('**Conversation started.**')
-            return
-        if message.content == '!end':
-            self.threads.pop(message.author)
-            await message.channel.send(
-                '**Conversation ended. I have forgotten everything.**')
-            return
-        try:
-            response = self.threads[message.author].ask(message.content)
-            responses = self._split_message(response)
-        except Exception as e:
-            print(e)
-            responses = ['**Something went wrong. Try again later.**']
-        finally:
-            for response in responses:
-                await message.channel.send(response, reference=message)
-
-    async def handle_channel(self, message):
-        atmessage = f"<@{self.dcbot_id}>"
-        if message.channel.id not in self.threads:
-            if message.content.strip(atmessage).strip() != '!start':
-                await message.channel.send(
-                    '**@Me and send `!start` to start a conversation\n@Me and send `!end` to end the conversation.\n\nI will forget everything after the conversation ends.**',
-                    reference=message)
-                return
-            self.threads[message.channel.id] = Chat(self.email,
-                                                    self.password,
-                                                    proxies=self.proxies)
-            await message.channel.send('**Conversation started.**',
-                                       reference=message)
-            return
-        if message.content.strip(atmessage).strip() == '!end':
-            self.threads.pop(message.channel.id)
-            await message.channel.send(
-                '**Conversation ended. I have forgotten everything.**',
-                reference=message)
-            return
-        try:
-            response = self.threads[message.channel.id].ask(
-                message.content.strip(atmessage))
-            responses = self._split_message(response)
-        except Exception as e:
-            print(e)
-            responses = ['Something went wrong. Try again later.']
-        finally:
-            for response in responses:
-                await message.channel.send(response, reference=message)
